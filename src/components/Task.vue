b5b6501953abd4ddfbe26e8d08eced5264e34669
emadhanna <emadhanna@gmail.com>
2022-04-14 19:45:43 -0400

    Adding the code for the Components Video

---
+++ src/components/Task.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <!-- <div>{{ task.name }} {{ index }}</div> -->
+    <div><slot></slot> {{ index }}</div>
+    <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+    <button @click="deleteTask(index)">X</button>
+  </li>
+</template>
+<script>
+  import { defineComponent } from '@vue/composition-api'
+
+  export default defineComponent({
+    props: ['task', 'index'],
+    setup() {
+
+    },
+  })
+</script>
+<style scoped>
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,120 +1,61 @@
 <template>
-  <!-- <q-page class="flex flex-center" padding> -->
   <q-page padding>
-    <button style="position: absolute; right: 10px" @click="counter++">{{ counter }}</button>
-    <!-- <input v-model="message" @keyup="handleKeyUp"/> -->
-    <!-- <input v-model="message" @keyup.esc="handleKeyUp" @keyup.enter="alertMessage" @mouseenter="alertMessage"/> -->
-    <!-- <input v-model="message" @keyup.esc="handleKeyUp" @keyup.enter="alertMessage" @mouseleave="alertMessage"/> -->
-    <!-- <input v-model="message" @keyup.esc="handleKeyUp" @keyup.enter="alertMessage" v-autofocus v-bind:class="{ 'error': message.length > 22}"/> -->
-    <input v-model="message" @keyup.esc="handleKeyUp" @keyup.enter="alertMessage" v-autofocus :style="errorStyle" ref="messageInput" />
-    <!-- <button @click="message= ''">Clear Message</button> -->
-    <button @click="clearMessage">Clear Message</button>
-    <div>{{ message.length }}</div>
-    <!-- <h5 class="border-grey" v-show="message.length">{{ message }}</h5> -->
-    <h5 class="border-grey" v-if="message.length">{{ message }}</h5>
-    <h6 v-else>No Message Entered</h6>
-
-    <hr>
-
-    <!-- <p>Uppercase Message: {{ messageUppercase() }}</p> -->
-    <p>Uppercase Message: {{ messageUppercase }}</p>
-    <!-- <p>Lowercase Message: {{ message | messageLowercase }}</p> -->
-    <p>Lowercase Message: {{ messageLowercase }}</p>
+    <ul>
+      <!-- <li v-for="(item, index) in tasks" :key="index">
+        <div>{{ item.name }} {{ index }}</div>
+        <small>{{ item.dueDate }} @ {{ item.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li> -->
+      <!-- <task v-for="(task, index) in tasks" :key="index" :task="task" :index="index"></task> -->
+      <task v-for="(task, index) in tasks" :key="index" :task="task" :index="index">{{ task.name }}</task>
+    </ul>
   </q-page>
 </template>
 
 <script>
+  import Task from 'components/Task.vue'
   import { defineComponent } from 'vue'
 
   export default defineComponent({
     name: 'IndexPage',
     data () {
       return {
-        message: 'I love Vue.Js Coding!',
-        counter: 0
+        tasks: [
+          {
+            name: 'Go To Shop',
+            dueDate: '2019/05/12',
+            dueTime: '18:30'
+          },
+          {
+            name: 'Get Bananas',
+            dueDate: '2019/05/13',
+            dueTime: '14:00'
+          },
+          {
+            name: 'get Apples',
+            dueDate: '2019/05/14',
+            dueTime: '16:00'
+          },
+        ]
       }
     },
     computed: {
-      messageUppercase() {
-        console.log('messageUpercase Was Fired')
-        return this.message.toUpperCase() + this.counter
-      },
-      messageLowercase() {
-        console.log('messageLowercase Was fired')
-        return this.message.toLowerCase() + this.counter
-      },
-      errorStyle() {
-        if (this.message.length > 22){
-          return {
-            'color': 'red',
-            'background': 'pink'
-          }
-        }
-      }
+
     },
     methods: {
-      clearMessage () {
-        this.message = ''
-      },
-      alertMessage (){
-        alert(this.message)
-      },
-      handleKeyUp (e){
-        console.log(e)
-        if (e.keyCode == 27) {
-          this.clearMessage()
-        }
-        else if (e.keyCode == 13){
-          this.alertMessage()
-        }
-      }
+      deleteTask(index) {
+        this.tasks.splice(index, 1)
+       }
+    },
+    components: {
+      task: Task
+      // 'task': require('components/Task.vue').default
     },
     directives: {
-      autofocus: {
-        inserted (element) {
-          element.focus()
-        }
-      }
-    },
-    // beforeCreate() {
-    //   console.log('beforeCreate!!!!')
-    // },
-    // beforeCreate() {
-    //   console.log('created!!!!')
-    // },
-    // beforeMount() {
-    //   console.log('beforeMount!!!!')
-    // },
-    mounted() {
-      console.log('mounted!!!!')
-      console.log(this.$refs)
-      this.$refs.messageInput.className = 'bg-green'
-    },
-    // beforeUpdate() {
-    //   console.log('beforeUpdate!!!!')
-    // },
-    // updated() {
-    //   console.log('Updated!!!!')
-    // },
-    // beforeDestroy() {
-    //   console.log('beforeDestroy!!!!')
-    // },
-    // destroyed() {
-    //   console.log('destroyed!!!!')
-    // }
+
+    }
   })
 </script>
 <style scoped>
-  .border-grey {
-    border: 1px solid grey;
-  }
 
-  input, button {
-    font-size: 23px;
-  }
-
-  .error {
-    color: red;
-    background: pink;
-  }
 </style>>
